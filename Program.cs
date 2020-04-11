9ec00108136c53392a29302baf71911879152dba
Aia Amin <aiaamin909.aa@gmail.com>
2020-04-11 16:45:51 +0200

    while

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace conditional_and_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int num=1;
+            while (num<6)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+            int x=0;
+            while(++x<6){
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
